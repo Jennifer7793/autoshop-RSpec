c9f6fcf22970418dbb520489b4e27c422db1083a
JenJen <jennifer7793@gmail.com>
2022-03-01 14:53:24 +0800

    feat: set order model

---
+++ app/models/order.rb
@@ -0,0 +1,16 @@
+class Order
+  attr_reader :items
+  def initialize
+    @items = []
+  end
+
+  def add_item(product_id)
+    found_item = items.find { |item| item.product_id == product_id }
+    
+    if found_item
+      found_item.increment
+    else
+      @items << OrderIem.new(product_id)
+    end
+  end
+end
